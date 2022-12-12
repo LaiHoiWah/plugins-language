da85e47df83f8e61dd6233738d894b74131f7be7
haihua <583268519@qq.com>
2022-12-12 10:36:57 +0800

    String equlas方法

---
+++ src/main/java/com/meowu/plugins/language/constants/StringConstants.java
@@ -0,0 +1,6 @@
+package com.meowu.plugins.language.constants;
+
+public interface StringConstants{
+
+    String EMPTY_STRING = "";
+}
--- src/main/java/com/meowu/plugins/language/utils/SpellingUtils.java
+++ src/main/java/com/meowu/plugins/language/utils/SpellingUtils.java
@@ -2,18 +2,17 @@ package com.meowu.plugins.language.utils;
 
 import com.google.common.base.CaseFormat;
 import com.meowu.commons.utils.AssertUtils;
+import com.meowu.plugins.language.constants.StringConstants;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
-public class SpellingUtils {
-
-    private final static String  EMPTY   = "";
-    private final static Charset CHARSET = Charset.forName("utf-8");
+public class SpellingUtils{
 
     public static String toString(byte[] bytes){
-        return toString(bytes, CHARSET);
+        return toString(bytes, StandardCharsets.UTF_8);
     }
 
     public static String toString(byte[] bytes, Charset charset){
@@ -21,7 +20,7 @@ public class SpellingUtils {
         AssertUtils.notNull(charset, "charset must not be null");
 
         if(ArrayUtils.isEmpty(bytes)){
-            return EMPTY;
+            return StringConstants.EMPTY_STRING;
         }
 
         return new String(bytes, charset);
@@ -61,5 +60,28 @@ public class SpellingUtils {
         AssertUtils.notNull(str, "string content must not be null");
 
         return CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, str);
+    }
+
+    public static boolean safeEqual(String compare, String compareTo){
+        if(compare == compareTo){
+            return true;
+        }
+
+        if(compare == null || compareTo == null){
+            return false;
+        }
+
+        if(compare.length() != compareTo.length()){
+            return false;
+        }
+
+        int result = 0;
+
+        // time-constant comparison
+        for(int i = 0; i < compare.length(); i++){
+            result |= compare.charAt(i) ^ compareTo.charAt(i);
+        }
+
+        return (result == 0);
     }
 }
